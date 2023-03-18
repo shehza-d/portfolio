cd6afaacc0ee796a0f95fcb4d356704b77387be9
Shehzad <shehzaddiqbal@gmail.com>
2023-03-18 22:57:25 +0500

    update

---
+++ notes.js
@@ -0,0 +1,5 @@
+/*
+<div class="w-[150px] h-[150px] rounded-full filter blur-3xl bg-custom-blue mt-20  md:ml-[130px] z-[-1] absolute"></div>
+<div class=" w-[200px] h-[200px] rounded-full filter blur-3xl bg-custom-blue/25  z-[-10] mt-[-160px] "></div>
+
+*/
