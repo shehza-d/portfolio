b652ab78956cacc271625d1622088ad0be82e020
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-04-10 00:30:29 +0500

    testing particle for responsive

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <main className={`dark:bg-[#0A192F]`}>
       <Cursor />
-      <Particle />
+      {/* <Particle /> */}
       <Observer />
       <Hero />
 
