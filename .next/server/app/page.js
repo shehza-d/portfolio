2f603fcc498086fdc6f221bb6d3b9b766653c805
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-06-04 23:48:08 +0500

    dp changed and console added

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -357,7 +357,7 @@ function Home() {
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [443,973,251], () => (__webpack_exec__(4615)));
+var __webpack_exports__ = __webpack_require__.X(0, [443,973,922], () => (__webpack_exec__(4615)));
 module.exports = __webpack_exports__;
 
 })();
